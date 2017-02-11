f4f3f159e4947f31a8117a0f9744bba292d952f7
Terence Huynh <me@terencehuynh.com>
2017-02-11 20:53:38 +1100

    Added konfig to app

--- env.js
+++ env.js
@@ -1,14 +1,21 @@
-exports.API_HOST = 'localhost';
-exports.API_PORT = '3000';
-exports.API_VERSION = '1.0.0';
-exports.API_URL_VERSION = '1';
-exports.TESTING = process.env.TESTING === true || process.env.TESTING === 'true';
-exports.PG_HOST = process.env.PG_HOST || 'localhost';
-exports.PG_PORT = process.env.PG_PORT || '5432';
-exports.PG_PASSWORD = process.env.PG_PASSWORD || '';
-exports.PG_USER = process.env.PG_USER || '';
-exports.PG_DATABASE = process.env.PG_DATABASE || 'skilled_acolyte_db';
+const config = require('konfig')();
 
-exports.DATABASE_URL = `postgres://${exports.PG_USER}:${exports.PG_PASSWORD}@${exports.PG_HOST}:${exports.PG_PORT}/${exports.PG_DATABASE}`;
+module.exports = {
+  // API Information
+  API_HOST: config.app.host,
+  API_PORT: config.app.port,
+  API_VERSION: config.app.version,
+  API_URL_VERSION: config.app.url_version,
 
-exports.DEV = process.env.DEV || false;
+  // PG Information
+  PG_HOST: process.env.PG_HOST || config.db.host,
+  PG_PORT: process.env.PG_PORT || config.db.port,
+  PG_USER: process.env.PG_USER || config.db.username,
+  PG_PASSWORD: process.env.PG_PASSWORD || config.db.password,
+  PG_DATABASE: process.env.PG_DATABASE || config.db.database,
+  DATABASE_URL: `postgres://${exports.PG_USER}:${exports.PG_PASSWORD}@${exports.PG_HOST}:${exports.PG_PORT}/${exports.PG_DATABASE}`,
+
+  // ENV Information
+  TESTING: process.env.TESTING === true || process.env.TESTING === 'true',
+  DEV: process.env.DEV || false,
+};
