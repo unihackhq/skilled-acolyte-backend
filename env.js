c805927657dcae1bdb7d3597cd0cd9769f4dd980
Terence Huynh <me@terencehuynh.com>
2017-02-11 22:06:03 +1100

    Use konfig to set up production config

--- env.js
+++ env.js
@@ -8,11 +8,11 @@ module.exports = {
   API_URL_VERSION: config.app.url_version,
 
   // PG Information
-  PG_HOST: process.env.PG_HOST || config.db.host,
-  PG_PORT: process.env.PG_PORT || config.db.port,
-  PG_USER: process.env.PG_USER || config.db.username,
-  PG_PASSWORD: process.env.PG_PASSWORD || config.db.password,
-  PG_DATABASE: process.env.PG_DATABASE || config.db.database,
+  PG_HOST: config.db.host,
+  PG_PORT: config.db.port,
+  PG_USER: config.db.username,
+  PG_PASSWORD: config.db.password,
+  PG_DATABASE: config.db.database,
   DATABASE_URL: `postgres://${exports.PG_USER}:${exports.PG_PASSWORD}@${exports.PG_HOST}:${exports.PG_PORT}/${exports.PG_DATABASE}`,
 
   // ENV Information
