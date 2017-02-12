0e91b9d7410381d66db53dc65a888477ffe92da9
Terence Huynh <me@terencehuynh.com>
2017-02-12 23:43:43 +1100

    Update config and DB connection settings

--- env.js
+++ env.js
@@ -1,19 +1,22 @@
 const config = require('konfig')();
 
+const APP = config.app;
+const DB = config.db;
+
 module.exports = {
   // API Information
-  API_HOST: config.app.host,
-  API_PORT: config.app.port,
-  API_VERSION: config.app.version,
-  API_URL_VERSION: config.app.url_version,
+  API_HOST: APP.host,
+  API_PORT: APP.port,
+  API_VERSION: APP.version,
+  API_URL_VERSION: APP.url_version,
 
   // PG Information
-  PG_HOST: config.db.host,
-  PG_PORT: config.db.port,
-  PG_USER: config.db.username,
-  PG_PASSWORD: config.db.password,
-  PG_DATABASE: config.db.database,
-  DATABASE_URL: `postgres://${exports.PG_USER}:${exports.PG_PASSWORD}@${exports.PG_HOST}:${exports.PG_PORT}/${exports.PG_DATABASE}`,
+  PG_HOST: DB.host,
+  PG_PORT: DB.port,
+  PG_USER: DB.username,
+  PG_PASSWORD: DB.password,
+  PG_DATABASE: DB.database,
+  DATABASE_URL: `postgres://${DB.username}:${DB.password}@${DB.host}:${DB.port}/${DB.database}`,
 
   // ENV Information
   TESTING: process.env.TESTING === true || process.env.TESTING === 'true',
