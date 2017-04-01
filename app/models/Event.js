4938514d8c6b23f988bb4c32521fe4b552c9fa54
Terence Huynh <me@terencehuynh.com>
2017-04-01 15:26:42 +1100

    Add Event, Student, Ticket and University model

---
+++ app/models/Event.js
@@ -0,0 +1,17 @@
+/*
+ * Represents a UNIHACK event. Contains basic information about the event, such
+ * as location, dates and Eventbrite link.
+ */
+module.exports = (sequelize, DataTypes) => sequelize.define('Event', {
+  id: {
+    primaryKey: true,
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+
+  },
+  name: DataTypes.STRING,
+  startDate: DataTypes.DATE,
+  endDate: DataTypes.DATE,
+  eventbriteLink: DataTypes.STRING,
+  location: DataTypes.STRING,
+});
