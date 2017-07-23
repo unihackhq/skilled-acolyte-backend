da49de69af89d326f5bb11a383fd7ff045e62323
Robert Koch <robert@kochie.io>
2017-07-23 13:39:01 +1000

    Added modifications to validators as requested.

--- app/validators/Team.js
+++ app/validators/Team.js
@@ -1,8 +1,8 @@
 const Joi = require('joi');
 
 exports.payload = {
-  name: Joi.string().required().error(new Error('name is required.')),
+  name: Joi.string(),
   description: Joi.string(),
   photoUrl: Joi.string().uri(),
-  EventId: Joi.string().uuid().required().error(new Error('EventId is required.')),
+  eventId: Joi.string().uuid(),
 };
