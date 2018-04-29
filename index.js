9f7f35ce6002c05b9cc26ec7f075df26f1f7cf09
erfanio <dev@erfan.io>
2018-04-29 16:27:07 +1000

    get the server to not crash instantly :P

--- index.js
+++ index.js
@@ -6,53 +6,41 @@ const routes = require('./app/routes');
 const models = require('./app/models');
 const validate = require('./app/util/Token');
 
-const app = new Hapi.Server();
+const init = async () => {
+  const app = new Hapi.Server({ host: env.API_HOST, port: env.API_PORT });
 
-// Define host and port
-app.connection({ host: env.API_HOST, port: env.API_PORT, routes: { cors: { origin: ['*'] } } });
-
-// TODO: Move to utils/
-
-// Register the JWT authentication plugin
-app.register([
-  {
-    register: require('hapi-auth-jwt2'),
-  },
-], (err) => {
-  if (err) throw err;
+  // Register the JWT authentication plugin
+  await app.register(require('hapi-auth-jwt2'));
 
   // JWT is the authentication strategy
   app.auth.strategy('jwt', 'jwt', {
     key: env.JWT_KEY,
-    validateFunc: validate,
+    validate,
     verifyOptions: { algorithms: ['HS256'] },
   });
 
   // Register the routes, stored in routes.js
   app.route(routes);
-});
 
-app.ext('onPreResponse', corsHeaders);
+  app.ext('onPreResponse', corsHeaders);
 
-// If we're not running tests, pretty print request/response
-// TODO: Use good, good-console and good-squeeze
-if (!env.TESTING) {
-  app.on('response', (request) => {
-    console.log(`Payload: ${JSON.stringify(request.payload)}`);
-    console.log(`${request.info.remoteAddress}: ${request.method.toUpperCase()} ${request.url.path} --> ${request.response.statusCode}`);
-  });
-}
+  // If we're not running tests, pretty print request/response
+  // TODO: Use good, good-console and good-squeeze
+  if (!env.TESTING) {
+    app.events.on('response', (request) => {
+      console.log(`Payload: ${JSON.stringify(request.payload)}`);
+      console.log(`${request.info.remoteAddress}: ${request.method.toUpperCase()} ${request.url.path} --> ${request.response.statusCode}`);
+    });
+  }
 
-// Start the app
-const options = {};
-if (env.DEV) {
-  options.force = true;
-}
-models.sequelize.sync(options).then(() => {
-  app.start((err) => {
-    if (err) throw err;
-    console.log(`Started. Running on http://${env.API_HOST}:${env.API_PORT}`);
-  });
-});
+  // Start the app
+  const options = {};
+  if (env.DEV) {
+    options.force = true;
+  }
+  await models.sequelize.sync(options);
+  await app.start();
+  console.log(`Started. Running on ${app.info.uri}`);
+};
 
-module.exports = app;
+init().catch(err => console.log(`Unexpected error ${err.message}\n`, err));
