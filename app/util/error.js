b1838ee40fbe12922b0658e5415946ae29a71295
erfanio <dev@erfan.io>
2018-05-21 14:09:16 +1000

    handle constraint errors and add details about which keys are not passing validation

--- app/util/error.js
+++ app/util/error.js
@@ -1,15 +1,29 @@
 const Boom = require('boom');
-const { ValidationError } = require('sequelize');
+const _ = require('lodash');
+const {
+  ValidationError,
+  ForeignKeyConstraintError,
+  ExclusionConstraintError,
+  UnknownConstraintError,
+} = require('sequelize');
 
 exports.preResponse = (req, h) => {
   const { response } = req;
 
-  // handle sequelize validation error
-  if (response instanceof ValidationError) {
+  // handle unknown validation error and other constraint errors
+  if (response instanceof ValidationError ||
+    response instanceof ExclusionConstraintError ||
+    response instanceof UnknownConstraintError ||
+    response instanceof ForeignKeyConstraintError) {
     const { table, detail } = response.parent;
 
-    const error = Boom.badRequest('Data validation failed', response);
+    const error = Boom.conflict('Data failed validation', response);
     error.output.payload.details = { table, detail };
+    if (response.errors) {
+      error.output.payload.details.errors =
+        response.errors.map(e => _.pick(e, ['message', 'type', 'path', 'value']));
+    }
+
 
     // log original error and return
     req.log('sequelize-error', response);
